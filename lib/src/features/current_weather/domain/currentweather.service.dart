f94ad0103d7e59b6741712c5441acc293e9db868
rodr15 <anthony.rodriguezf11@gmail.com>
2025-11-17 09:18:47 -0500

    (update) Documentation: documentate weather service

--- lib/src/features/current_weather/domain/currentweather.service.dart
+++ lib/src/features/current_weather/domain/currentweather.service.dart
@@ -1,5 +1,9 @@
 import 'package:weather_app_pt/src/shared/domain/models/place.dart';
 
+/// {@template weather_service}
+/// Abstract class defining the contract for fetching weather-related data.
+/// {@endtemplate}
 abstract class WeatherService {
+  /// Fetches the current weather for the user's current location.
   Future<Place> getCurrentPlaceWeather();
 }
